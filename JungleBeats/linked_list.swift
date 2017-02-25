2ce21993b30fbfe4a5cb3b9966344e3025a32d1c
Mark Miranda <notmarkmiranda@gmail.com>
2017-02-25 13:49:01 -0700

    iteration 3

--- JungleBeats/linked_list.swift
+++ JungleBeats/linked_list.swift
@@ -14,6 +14,9 @@ class LinkedList {
         let newNode = Node(data: data)
         if head == nil {
             head = newNode
+        } else {
+            let current = findEndNode()
+            current.next_node = newNode
         }
         return data
     }
@@ -30,16 +33,89 @@ class LinkedList {
         return count
     }
     
-    func to_string() -> String{
+    func find(position: Int, words: Int) -> String {
+        var listString = ""
+        var current = goToNodePosition(position: position + 1)
+        for _ in 1...words {
+            listString += "\(current.data) "
+            current = current.next_node!
+        }
+        return listString.trimmingCharacters(in: .whitespaces)
+    }
+    
+    func includes(data: String, current_node: Node? = nil) -> Bool {
+        let current = current_node ?? head!
+        if current.data == data {
+            return true
+        } else if current.next_node != nil {
+            return includes(data: data, current_node: current.next_node)
+        }
+        return false
+    }
+    
+    func insert(position: Int, data: String) -> String {
+        let newNode = Node(data: data)
+        let current = goToNodePosition(position: position)
+        let temp = current.next_node
+        current.next_node = newNode
+        newNode.next_node = temp
+        return data
+    }
+    
+    func pop() -> String {
+        let secondLast = secondToLast()
+        let nodeString = secondLast.next_node!.data
+        secondLast.next_node = nil
+        return nodeString
+    }
+    
+    func prepend(data: String) -> String {
+        let newNode = Node(data: data)
+        if head != nil {
+            newNode.next_node = head!
+            
+        }
+        head = newNode
+        return data
+    }
+    
+    func toString() -> String{
         var listString = ""
         if var current = head {
             listString += "\(current.data) "
             while current.next_node != nil {
-                listString += current.data + " "
                 current = current.next_node!
+                listString += current.data + " "
             }
         }
-        
         return listString.trimmingCharacters(in: .whitespaces)
     }
+    
+    private func findEndNode() -> Node {
+        var current = head!
+        while current.next_node != nil {
+            current = current.next_node!
+        }
+        return current
+    }
+    
+    private func goToNodePosition(position: Int) -> Node {
+        var current = head!
+        var i = 1
+        while i < position {
+            current = current.next_node!
+            i += 1
+        }
+        return current
+    }
+    
+    private func secondToLast(node : Node? = nil) -> Node {
+        var current = head!
+        while current.next_node?.next_node != nil {
+            current = current.next_node!
+        }
+        return current
+    }
+ 
+
 }
--- JungleBeatsTests/linked_list_test.swift
+++ JungleBeatsTests/linked_list_test.swift
@@ -26,13 +26,109 @@ class LinkedListTest: XCTestCase {
     func test_list_can_count() {
         let list = LinkedList()
         XCTAssertEqual(list.count(), 0)
-        list.append(data: "doop")
+        _=list.append(data: "doop")
         XCTAssertEqual(list.count(), 1)
     }
     
     func test_list_can_to_string() {
         let list = LinkedList()
-        list.append(data: "doop")
-        XCTAssertEqual(list.to_string(), "doop")
+        _=list.append(data: "doop")
+        XCTAssertEqual(list.toString(), "doop")
+    }
+    
+    func test_list_can_append_twice() {
+        let list = LinkedList()
+        _=list.append(data: "doop")
+        XCTAssertNotNil(list.head)
+        XCTAssertNil(list.head?.next_node)
+        
+        XCTAssertEqual(list.append(data: "deep"), "deep")
+        XCTAssertNotNil(list.head?.next_node)
+    }
+    
+    func test_list_can_count_two_nodes() {
+        let list = LinkedList()
+        _=list.append(data: "doop")
+        _=list.append(data: "deep")
+        XCTAssertEqual(list.count(), 2)
+    }
+    
+    func test_list_can_to_string_two_nodes() {
+        let list = LinkedList()
+        _=list.append(data: "doop")
+        _=list.append(data: "deep")
+        XCTAssertEqual(list.toString(), "doop deep")
+    }
+    
+    func test_list_can_prepend() {
+        let list = LinkedList()
+        _=list.append(data: "doop")
+        XCTAssertEqual(list.toString(), "doop")
+        
+        _=list.append(data: "suu")
+        XCTAssertEqual(list.toString(), "doop suu")
+        
+        XCTAssertEqual(list.prepend(data: "dop"), "dop")
+        XCTAssertEqual(list.toString(), "dop doop suu")
+        XCTAssertEqual(list.count(), 3)
+    }
+    
+    func test_list_can_insert() {
+        let list = LinkedList()
+        _=list.append(data: "third")
+        _=list.append(data: "fourth")
+        _=list.prepend(data: "first")
+        XCTAssertEqual(list.insert(position: 1, data: "second"), "second")
+        XCTAssertEqual(list.toString(), "first second third fourth")
+    }
+    
+    func test_list_can_find() {
+        let list = LinkedList()
+        _=list.append(data: "first")
+        _=list.append(data: "second")
+        _=list.append(data: "third")
+        _=list.append(data: "fourth")
+        _=list.append(data: "fifth")
+        
+        XCTAssertEqual(list.toString(), "first second third fourth fifth")
+        XCTAssertEqual(list.find(position: 2, words: 1), "third")
+    }
+    
+    func test_list_can_find_multiple_words() {
+        let list = LinkedList()
+        _=list.append(data: "first")
+        _=list.append(data: "second")
+        _=list.append(data: "third")
+        _=list.append(data: "fourth")
+        _=list.append(data: "fifth")
+        XCTAssertEqual(list.find(position: 1, words: 3), "second third fourth")
+    }
+    
+    func test_list_can_check_for_includes_true() {
+        let list = LinkedList()
+        _=list.append(data: "first")
+        _=list.append(data: "second")
+        _=list.append(data: "third")
+        _=list.append(data: "fourth")
+        _=list.append(data: "fifth")
+        XCTAssert(list.includes(data: "first"))
+        XCTAssert(list.includes(data: "fourth"))
+    }
+    
+    func test_list_includes_can_return_false() {
+        let list = LinkedList()
+        _=list.append(data: "first")
+        XCTAssertFalse(list.includes(data: "second"))
+    }
+    
+    func test_list_can_pop() {
+        let list = LinkedList()
+        _=list.append(data: "first")
+        _=list.append(data: "second")
+        _=list.append(data: "third")
+        _=list.append(data: "fourth")
+        XCTAssertEqual(list.pop(), "fourth")
+        XCTAssertEqual(list.pop(), "third")
+        XCTAssertEqual(list.count(), 2)
     }
 }
